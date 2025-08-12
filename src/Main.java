2d005025d9cc3a34584eccb8e3b6aba50893a70f
emanuelhonorio <43287989+emanuelhonorio@users.noreply.github.com>
2025-08-12 07:57:20 -0300

    squase one

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import controllers.GameController;
 public class Main {
     public static void main(String[] args) {
         GameController game = new GameController();
-        System.out.println("Inicio do jogo");
+        System.out.println("Inicio do jogo 2");
         game.jogar();
         game.sair();
     }
